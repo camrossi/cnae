c2b128487e2c5c9530f4da1db63be8469af29e69
camrossi <camillo.rossi@gmail.com>
2019-10-01 18:58:56 +0200

    Added code to prepare an appliance for demos

--- cnae.py
+++ cnae.py
@@ -1,22 +1,23 @@
 import requests
 import sys
+import os
 import logging
 import json
 from pprint import pprint
 import time
 import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from selenium import webdriver
 
 class NAE:
     def __init__(self, ip_addr):
         self.ip_addr = ip_addr
         self.session_cookie = {}
         self.assuranceGroups = {}
+        self.files = {}
         self.http_header = {'Host' : self.ip_addr,'Content-type':'application/json;charset=utf-8'}
-
         # create logger
         self.logger = logging.getLogger(__name__)
-        
         
         # create console handler and set level to debug
         self.ch = logging.StreamHandler()
@@ -76,13 +77,48 @@ class NAE:
         if req.status_code == 200:
             self.assuranceGroups = req.json()['value']['data']
             self.logger.debug("Update all the assurange groups data")
-
+            
     def getAG(self, name):
         self.getAllAG()
         for ag in self.assuranceGroups:
             if ag['unique_name'] == name:
                 return ag
         return None
+    
+    def getFirstAG(self):
+        # Some API requires an Assurance grup in the API call even if does not matter which AG you select
+        # For this I have created this methodggGG
+        self.getAllAG()
+        return self.assuranceGroups[0]        
+        
+
+    def newOfflineAG(self, name):
+        # This method creates a new Offline Assurance Group, you only need to pass the AG Name.
+
+        url=  'https://'+self.ip_addr+'/api/v1/config-services/assured-networks/aci-fabric/'
+
+        form ='''{
+          "analysis_id": "",
+          "display_name": "",
+          "description": "",
+          "operational_mode": "OFFLINE",
+          "status": "STOPPED",
+          "active": true,
+          "unique_name": "''' + name + '''",
+          "assured_network_type": "",
+          "analysis_timeout_in_secs": 3600,
+          "apic_configuration_export_policy": {
+            "apic_configuration_export_policy_enabled": false,
+            "export_format": "XML",
+            "export_policy_name": ""
+          },
+          "analysis_schedule_id": ""}'''
+       
+        req = requests.post(url, data=form,  headers=self.http_header, cookies=self.session_cookie, verify=False)
+        if req.status_code == 201:
+             self.logger.info("Assurange group %s created", name)
+        else:
+             self.logger.info("Assurange Group creation failed with error message \n %s",req.json())
 
     def isLiveAnalysis(self):
         self.getAllAG()
@@ -98,7 +134,6 @@ class NAE:
                 self.logger.debug("There is a Running OnDemand Analysis on Assurance Group %s",ag['unique_name'])
                 return ag['unique_name']
             
-
 
     def StartOnDemandAnalysis(self, ag_name, iterations):
 
@@ -121,6 +156,7 @@ class NAE:
                 self.logger.info("The %s assurance group does not exist",ag_name)
                 exit()
 
+
             ag_iterations = json.dumps({'iterations': iterations})
             url = 'https://'+self.ip_addr+'/api/v1/config-services/assured-networks/aci-fabric/'+ag['uuid']+'/start-analysis'
             req = requests.post(url, data=ag_iterations, headers=self.http_header,cookies=self.session_cookie, verify=False)
@@ -131,6 +167,35 @@ class NAE:
                 self.logger.info('OnDemand Analysis failed to start on %s.', ag_name)
                 pprint(req.json())
                 exit()
+
+    def newOfflineAnalysis(self, name, fileID, fabricID):
+        self.logger.info("Trying to Starting Analysis  %s",name)
+        
+        while self.isOnDemandAnalysis() or self.isLiveAnalysis():
+            self.logger.info("There is currently an  analysis running  will try again in 2 minutes. \n If is a live analysis please stop it manually")
+            time.sleep(120)
+
+        form = '''{
+          "unique_name": "''' + name + '''",
+          "file_upload_uuid": "''' + fileID +'''",
+          "aci_fabric_uuid": "''' + fabricID + '''",
+          "analysis_timeout_in_secs": 3600
+        }'''
+        url ='https://'+self.ip_addr+'/api/v1/event-services/offline-analysis'
+        req = requests.post(url, data=form,  headers=self.http_header, cookies=self.session_cookie, verify=False)
+        pprint(req)
+        if req.status_code == 202:
+            self.logger.info("Offline Analysis %s Started", name)
+        else:
+            self.logger.info("Offline Analysis creation failed with error message \n %s",req.content)
+
+    def getFiles(self):
+        #This methods loads all the uploaded files to NAE
+        url = 'https://'+self.ip_addr+'/api/v1/file-services/upload-file'
+        req = requests.get(url, headers=self.http_header, cookies=self.session_cookie, verify=False)
+        self.files = req.json()['value']['data']
+
+
     def updateLicense(self,license):
         pass
 
@@ -146,23 +211,49 @@ class NAE:
         if current_ondemand == ag_name:
             pass
         
-
-        
-        
-
-
-
-
-
-
-
-
-
-
-
-
+    def getObjectSelector(self, os_name, ag_name):
+        # Get all object selectors and return them as a dictionary
+        ag = self.getAG(ag_name)
+        #Get the list of Object Selectors 
+        url = 'https://'+self.ip_addr+'/api/v1/event-services/assured-networks/' + ag + '/model/aci-policy/compliance-requirement/object-selectors'
+        req = requests.get(url, headers=self.http_header, cookies=self.session_cookie, verify=False)
+        os_list = req.response()
+        pprint(os_list)
 
     
+    def newObjectSelector(self, form):
+        ag = self.getFirstAG()
+        url ='https://'+self.ip_addr+'/api/v1/event-services/assured-networks/'+ag["uuid"]+'/model/aci-policy/compliance-requirement/object-selectors'
+        req = requests.post(url, data=form,  headers=self.http_header, cookies=self.session_cookie, verify=False)
+        if req.status_code == 200:
+           self.logger.info("Object Selectors created")
+        else:
+           self.logger.info("Object Selectors creation failed with error message \n %s",req.json())
 
+    def newTrafficSelector(self, form):
+        ag = self.getFirstAG()
+        url ='https://'+self.ip_addr+'/api/v1/event-services/assured-networks/'+ag["uuid"]+'/model/aci-policy/compliance-requirement/traffic-selectors'
+        req = requests.post(url, data=form,  headers=self.http_header, cookies=self.session_cookie, verify=False)
+        if req.status_code == 200:
+           self.logger.info("Traffic Selectors created")
+        else:
+           self.logger.info("Traffic Selectors creation failed with error message \n %s",req.json())
 
+    def newComplianceRequirement(self, form):
+        ag = self.getFirstAG()
+        url ='https://'+self.ip_addr+'/api/v1/event-services/assured-networks/'+ag["uuid"]+'/model/aci-policy/compliance-requirement/requirements'
+        req = requests.post(url, data=form,  headers=self.http_header, cookies=self.session_cookie, verify=False)
+        if req.status_code == 200:
+           self.logger.info("Complianc Requirement created")
+        else:
+           self.logger.info("Complianc Requirement creation failed with error message \n %s",req.json())
 
+    def newComplianceRequirementSet(self, form):
+        ag = self.getFirstAG()
+        url = 'https://'+self.ip_addr+'/api/v1/event-services/assured-networks/'+ag["uuid"]+'/model/aci-policy/compliance-requirement/requirement-sets'
+        req = requests.post(url, data=form,  headers=self.http_header, cookies=self.session_cookie, verify=False)
+        if req.status_code == 200:
+           self.logger.info("Complianc Requirement Set created")
+        else:
+           self.logger.info("Complianc Requirement Set creation failed with error message \n %s",req.json())
+    
---
+++ demo_config.py
@@ -0,0 +1,248 @@
+# This script will pre-configure a NAE appliance for some demo time.
+# I expect the appliance to be with no config, if an object already exist I will not update it, the object creation will just fail and I move on. 
+# To do: Move the configuration snippet outside of this file.
+import cnae
+import logging
+import time
+import argparse
+import getpass
+
+
+# Enble logging at debug level
+logger = logging.getLogger('cnae')
+logger.setLevel(logging.INFO)
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Prepare an NAE appliace for Demo Time!")
+    parser.add_argument('-u', dest='user', help='Username, default: admin', default='admin')
+    parser.add_argument('-d', dest='domain', help='Login Domain, defaul: Local',default='Local')
+    parser.add_argument('-i', dest='nae_ip', help='IP address of the NAE Appliance',required=True)
+    args = parser.parse_args()
+    return args
+
+
+args= get_args()
+nae_password = getpass.getpass()
+
+#Create NAE Object
+nae = cnae.NAE (args.nae_ip)
+
+#Log in to NAE with user and password
+nae.login(args.user, nae_password,args.domain)
+
+#Create assurange Groups
+nae.newOfflineAG("Segmentation")
+nae.newOfflineAG("Segmentation_SLA")
+nae.newOfflineAG("Can5")
+
+object_selectors=[
+                  '''{
+                        "name": "DataBase",
+                    "description": null,
+                    "includes": [
+                      {
+                        "matches": [
+                          {
+                            "application_epgmatch": {
+                              "object_attribute": "DN",
+                              "tenant": {
+                                "pattern": "NAE_Compliance",
+                                "type": "EXACT"
+                              },
+                              "application_profile": {
+                                "pattern": "ComplianceIsGood",
+                                "type": "EXACT"
+                              },
+                              "application_epg": {
+                                "pattern": "DataBase",
+                                "type": "EXACT"
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    ],
+                    "excludes": [],
+                    "selector_type": "OST_EPG"
+                  }''',
+                  '''{
+                    "name": "FrontEnd",
+                    "description": null,
+                    "includes": [
+                      {
+                        "matches": [
+                          {
+                            "application_epgmatch": {
+                              "object_attribute": "DN",
+                              "tenant": {
+                                "pattern": "NAE_Compliance",
+                                "type": "EXACT"
+                              },
+                              "application_profile": {
+                                "pattern": "ComplianceIsGood",
+                                "type": "EXACT"
+                              },
+                              "application_epg": {
+                                "pattern": "FrontEnd",
+                                "type": "EXACT"
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    ],
+                    "excludes": [],
+                    "selector_type": "OST_EPG"
+                  }''',
+                  '''{
+                    "name": "WebService",
+                    "description": null,
+                    "includes": [
+                      {
+                        "matches": [
+                          {
+                            "application_epgmatch": {
+                              "object_attribute": "DN",
+                              "tenant": {
+                                "pattern": "NAE_Compliance",
+                                "type": "EXACT"
+                              },
+                              "application_profile": {
+                                "pattern": "ComplianceIsGood",
+                                "type": "EXACT"
+                              },
+                              "application_epg": {
+                                "pattern": "WebService",
+                                "type": "EXACT"
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    ],
+                    "excludes": [],
+                    "selector_type": "OST_EPG"
+                  }'''
+]
+for obj in object_selectors:
+    nae.newObjectSelector(obj)
+
+traffic_selectos = [
+                    '''{
+                      "name": "MySQL",
+                      "includes": {
+                        "selectors": [
+                          {
+                            "selectors": [
+                              {
+                                "ether_type": "IP",
+                                "ip_protocol": "TCP",
+                                "reverse_port": true,
+                                "port_selector_ato_b": {
+                                  "dst_port": "3306",
+                                  "tcp_flag": false,
+                                  "tcp_flags_set": [],
+                                  "tcp_flags_not_set": []
+                                }
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    }'''
+                    ]
+
+for obj in traffic_selectos:
+    nae.newTrafficSelector(obj)
+
+compliance_requirements = [
+
+                            '''{
+                              "name": "SLA",
+                              "config_compliance_parameter": {
+                                "and_list_list": [],
+                                "or_list_list": []
+                              },
+                              "epgselector_a": "WebService",
+                              "epgselector_b": "DataBase",
+                              "traffic_selector": "MySQL",
+                              "requirementType": "SLA",
+                              "communicationType": "MUST",
+                              "isAllTraffic": false
+                            }''','''
+                            {
+                                  "name": "Segmentation_Epoch",
+                                  "config_compliance_parameter": {
+                                    "and_list_list": [],
+                                    "or_list_list": []
+                                  },
+                                  "epgselector_a": "FrontEnd",
+                                  "epgselector_b": "DataBase",
+                                  "requirementType": "SEGMENTATION",
+                                  "communicationType": "MUST_NOT",
+                                  "isAllTraffic": false
+                            }'''
+    ]
+
+for obj in compliance_requirements:
+    nae.newComplianceRequirement(obj)
+
+seg_sla = nae.getAG('Segmentation_SLA')
+seg = nae.getAG('Segmentation')
+requirement_sets = [
+                    '''{
+                        "assurance_groups": [
+                            {
+                                "active": true,
+                                "fabric_uuid": "''' + seg_sla['uuid'] + '''"
+
+                            }
+                        ],
+                        "description": null,
+                        "name": "Segmentation_SLA",
+                        "requirements": [
+                            "SLA",
+                            "Segmentation_Epoch"
+                        ]
+                    }''','''
+                    {
+                        "assurance_groups": [
+                            {
+                                "active": true,
+                                "fabric_uuid": "''' + seg['uuid'] + '''"
+
+                            }
+                        ],
+                        "description": null,
+                        "name": "Segmentation",
+                        "requirements": [
+                            "Segmentation_Epoch"
+                        ]
+                    }'''
+]
+
+for obj in requirement_sets:
+    nae.newComplianceRequirementSet(obj)
+
+
+offline_analysis = [{"ag":"Can5", "filename": ["Can5-Epoch1.gz", "Can5-Epoch2.gz", "Can5-Epoch3.gz" ]},
+                    {"ag":"Segmentation", "filename": ["Segmentation_SLA_Epoch1.tar.gz", "Segmentation_SLA_Epoch2.tar.gz", "Segmentation_SLA_Epoch3.tar.gz", "Segmentation_SLA_Epoch4.gz" ]},
+                    {"ag":"Segmentation_SLA", "filename": ["Segmentation_Epoch1.tar.gz", "Segmentation_Epoch2.tar.gz", "Segmentation_Epoch3.tar.gz" ]}]
+              
+#Load the list of Offline dataset
+nae.getFiles()
+
+#for all the assurance group 
+for ag in offline_analysis:
+    #Get the fabricID
+    fabricID = str(nae.getAG(ag['ag'])['uuid'])
+    #for all the files in the assurange group list
+    for f in ag['filename']:
+        #genarate the name from the file (remving the extension)
+        offline_analysis_name = f.split('.')[0]
+        # Get the file ID from the file name
+        fileID = str(next(item for item in nae.files if item["filename"] == f)['uuid'])
+        #Start a new Offline Analysis
+        nae.newOfflineAnalysis(offline_analysis_name, fileID, fabricID)
+
+
